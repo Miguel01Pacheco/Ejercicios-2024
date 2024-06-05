97a74d69a44030ca8d72da13baa4944174c2a7e6
Miguel01Pacheco <miguemap01@gmail.com>
2024-06-05 01:05:35 -0300

    logica terminada

--- Ejercicios-Varios/calculadora_con_botones/scripts/app.js
+++ Ejercicios-Varios/calculadora_con_botones/scripts/app.js
@@ -17,14 +17,15 @@ const $multiplicacion = document.getElementById("multiplicacion");
 const $division = document.getElementById("division");
 const $atras = document.getElementById("pop");
 const $igual = document.getElementById("igual");
+const $clear = document.getElementById("clear");
 //Declaro los arrays necesarios para dos nuneros
 //y los que funcionan como swicht para los while.
 const numero1 =[];
 const numero2 =[];
-const primerNumero = true;
-const segundoNumero = false;
-const operacion = "+";
-const resultado = [];
+let primerNumero = true;
+let segundoNumero = false;
+let operacion = "";
+let resultado = 0;
 
 //Funcion calcular
 function calcular(num1,num2,op){
@@ -43,86 +44,132 @@ function calcular(num1,num2,op){
     $uno.addEventListener("click", (e)=>{
         if(primerNumero){
         numero1.push(1);
+        $pantalla.innerText =`${numero1.join("")}`;
         }else if (segundoNumero){
-        numero2.push(1);}
+        numero2.push(1);
+        $pantalla.innerText =`${numero1.join("")} ${operacion} ${numero2.join("")}`;}
     });
     ;$dos.addEventListener("click", (e)=>{
         if(primerNumero){
         numero1.push(2);
+        $pantalla.innerText =`${numero1.join("")}`;
         }else if(segundoNumero){
-        numero2.push(2);    
+        numero2.push(2);
+        $pantalla.innerText =`${numero1.join("")} ${operacion}${numero2.join("")}`;    
         }
     });$tres.addEventListener("click", (e)=>{
         if(primerNumero){
         numero1.push(3);
+        $pantalla.innerText =`${numero1.join("")}`;
         }else if (segundoNumero){
-        numero2.push(3);    
+        numero2.push(3);
+        $pantalla.innerText =`${numero1.join("")} ${operacion}${numero2.join("")}`;    
         }
     });$cuatro.addEventListener("click", (e)=>{
         if(primerNumero){
         numero1.push(4);
+        $pantalla.innerText =`${numero1.join("")}`;
         }else if (segundoNumero){
-        numero2.push(4);    
+        numero2.push(4);
+        $pantalla.innerText =`${numero1.join("")} ${operacion}${numero2.join("")}`;    
         }
     });$cinco.addEventListener("click", (e)=>{
         if (primerNumero){
             numero1.push(5);
+            $pantalla.innerText =`${numero1.join("")}`;
         }else if (segundoNumero){
-        numero2.push(5);    
+        numero2.push(5);
+        $pantalla.innerText =`${numero1.join("")} ${operacion}${numero2.join("")}`;    
         }   
     });$seis.addEventListener("click", (e)=>{
         if (primerNumero){
         numero1.push(6);
+        $pantalla.innerText =`${numero1.join("")}`;
         }else if (segundoNumero){
-        numero2.push(6);    
+        numero2.push(6);
+        $pantalla.innerText =`${numero1.join("")} ${operacion}${numero2.join("")}`;     
         }
     });$siete.addEventListener("click", (e)=>{
         if (primerNumero){
         numero1.push(7);
+        $pantalla.innerText =`${numero1.join("")}`;
         }else if (segundoNumero){
-        numero2.push(7);    
+        numero2.push(7);
+        $pantalla.innerText =`${numero1.join("")} ${operacion}${numero2.join("")}`;    
         }
     });$ocho.addEventListener("click", (e)=>{
         if (primerNumero){
         numero1.push(8);
+        $pantalla.innerText =`${numero1.join("")}`;
         }else if (segundoNumero){
-        numero2.push(8);    
+        numero2.push(8);
+        $pantalla.innerText =`${numero1.join("")} ${operacion}${numero2.join("")}`;    
         }
     });$nueve.addEventListener("click", (e)=>{
         if (primerNumero){
         numero1.push(9);
+        $pantalla.innerText =`${numero1.join("")}`;
         }else if (segundoNumero){
-        numero2.push(9);    
+        numero2.push(9);
+        $pantalla.innerText =`${numero1.join("")} ${operacion}${numero2.join("")}`;    
         }
     });$cero.addEventListener("click", (e)=>{
         if (primerNumero){
         numero1.push(0);
+        $pantalla.innerText =`${numero1.join("")}`;
         }else if (segundoNumero){
-        numero2.push(0);    
+        numero2.push(0);
+        $pantalla.innerText =`${numero1.join("")} ${operacion}${numero2.join("")}`;    
         }
     });
 //Escucho los botones de operaciones    
 $suma.addEventListener("click",(e)=>{
+    operacion = undefined;
     operacion = "+";
+    $pantalla.innerText =`${numero1.join("")} ${operacion}`;
     primerNumero= false;
     segundoNumero = true ;
 });   
 $resta.addEventListener("click",(e)=>{
+    operacion = undefined;
     operacion = "-";
+    $pantalla.innerText =`${numero1.join("")} ${operacion}`;
     primerNumero = false;
     segundoNumero = true ;
 });  $multiplicacion.addEventListener("click",(e)=>{
+    operacion = undefined ;
     operacion = "*";
+    $pantalla.innerText =`${numero1.join("")} ${operacion}`;
     primerNumero = false;
     segundoNumero = true ;
 });  $division.addEventListener("click",(e)=>{
+    operacion = undefined ;
     operacion = "/";
+    $pantalla.innerText =`${numero1.join("")} ${operacion}`;
     primerNumero = false;
     segundoNumero = true ;
-});  
+}); 
+$atras.addEventListener("click",(e)=>{
+    if (primerNumero){
+        numero1.pop();
+        $pantalla.innerText =`${numero1.join("")}`;
+    }else if (segundoNumero){
+        numero2.pop();
+        $pantalla.innerText =`${numero2.join("")}`;
+    }
+
+}); 
 $igual.addEventListener("click",(e)=>{
     primerNumero = true;
     segundoNumero = false;
-    resultado = calcular(numero1,numero2,operacion);
+    resultado = calcular(parseInt(numero1.join("")),parseInt(numero2.join("")),operacion);
     $pantalla.innerText =`${resultado}`; 
 }); 
+$clear.addEventListener("click",(e)=>{
+    $pantalla.innerText ="";
+    operacion =undefined;
+    numero1.length =0;
+    numero2.length = 0;
+    primerNumero =true;
+    segundoNumero =false;
+});
