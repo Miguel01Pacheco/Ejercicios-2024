cea12a08f8857b1502c6c6f489f45e9260bdc553
Miguel01Pacheco <miguemap01@gmail.com>
2024-06-11 00:17:49 -0300

    update scripts and styles

--- Ejercicios-UTN/segundo-tp/scripts/app.js
+++ Ejercicios-UTN/segundo-tp/scripts/app.js
@@ -34,7 +34,21 @@ function house(valor){
   }
   return valor;
 }
-
+//Funcion para detectar si el personaje tiene ancestros.Si el campo esta vacio no se muestra.
+function ancestry(valor){
+    if (valor ==="half-blood"){
+        return "Half-Blood";
+    }else if (valor === "pure-blood"){
+        return "Pure-Blood";
+    }else if (valor === "muggle"){
+        return "Muggle";
+    }else if(valor ==="muggleborn"){
+        return "MuggleBorn";
+    }else if (valor==="quib"){
+        return "Quib";
+    }
+    return "";
+}
 //Mediante un fecth obtengo los datos desde la API de Harry Pother.
 fetch("https://hp-api.onrender.com/api/characters")
   .then((respuesta) => {
@@ -44,13 +58,12 @@ fetch("https://hp-api.onrender.com/api/characters")
     let personajes = data;
     console.log(personajes);    
 //Bucle para filtrar los personajes que no tienen imagen
-/*
+/*Decidí no usarla pero la dejo para ilustrar.
    personajes = personajes.filter((personaje)=>
 {
     if (personaje.image !=="")
         return personaje;
 })
-console.log(personajes);
 */
 //Array para guardar el contenido html.
 let contenidoHtml =[];        
@@ -61,7 +74,7 @@ let contenidoHtml =[];
       <h1 class="titulo">${personaje.name}</h1>
       <img  class="image" src="${imageOk(personaje.image,personaje.gender)}" alt="imagen de ${personaje.name}">
       <p class = "${house(personaje.house)}" > ${house(personaje.house)}</p>
-      <p  class ="ancestry">${personaje.ancestry}</p>
+      <p  class =${ancestry(personaje.ancestry)}>${ancestry(personaje.ancestry)}</p>
       <p class ="wizard">${esMago(personaje.wizard)}</p>
       </div>
       `;
