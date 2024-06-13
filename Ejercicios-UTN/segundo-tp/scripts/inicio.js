6b000163eff5b8e3a367b4a0e61892a3183b2b59
Miguel01Pacheco <miguemap01@gmail.com>
2024-06-12 23:32:28 -0300

    more update styles

--- Ejercicios-UTN/segundo-tp/scripts/inicio.js
+++ Ejercicios-UTN/segundo-tp/scripts/inicio.js
@@ -2,12 +2,12 @@ const $casas = document.getElementById("houses");
 $casas.innerHTML = `
 <div class="cont-img">
 <p class="gri"> <img src="assets/gryffindor-sin-fondo.png">
-La casa de Gryffindor es confortable, con paredes rojas y doradas, decorada con trofeos y retratos de valientes hechiceros.</p>
+Gryffindor es confortable, con paredes rojas y doradas, decorada con trofeos y retratos de valientes hechiceros.</p>
                    <p class="sli"> <img src="assets/slytherin-sin-fondo.png">
-La casa de Slytherin es elegante, con paredes verdes y plateadas, adornada con símbolos de ambición y astucia.</p>
-                   <p class="rav"> <img src="assets/ravenclaw-sin-fondo.png">La casa de Ravenclaw es intelectual, con paredes azules y bronce, resplandeciendo con libros antiguos y objetos curiosos</p>
+Slytherin es elegante, con paredes verdes y plateadas, adornada con símbolos de ambición y astucia.</p>
+                   <p class="rav"> <img src="assets/ravenclaw-sin-fondo.png">Ravenclaw es intelectual, con paredes azules y bronce, resplandeciendo con libros antiguos y objetos curiosos</p>
                    <p class="huf"> <img src="assets/hufflepuff-sin-fondo.png">
-La casa de Hufflepuff es acogedora, con paredes amarillas y negras, iluminada con la calidez de la amistad y la lealtad.</p>
+Hufflepuff es acogedora, con paredes amarillas y negras, iluminada con la calidez de la amistad y la lealtad.</p>
                    </div>`;
 
 
